cafe3b6a294d94202f9e8b7f64904aabefc2067d
Bevin Duncan <bevin.duncan@gmail.com>
2022-02-05 13:23:57 -0800

    Completed Assignment

--- index.js
+++ index.js
@@ -1,17 +1,11 @@
 function receivesAFunction(cb){
-    console.log(cb());
+    return cb();
 }
-receivesAFunction(function () {return "this is crazy"})
 
 function returnsANamedFunction(){
-   return function namedFunction(){}
-}
-function returnsAnAnonymousFunction(){
-    return function(){
-        console.log("Seriously, this is crazy what have I gotten myself in to??")
-    }
+    return function namedFunction(){}
 }
 
-
-
-    
+ function returnsAnAnonymousFunction(){
+     return function(){}
+ }   
